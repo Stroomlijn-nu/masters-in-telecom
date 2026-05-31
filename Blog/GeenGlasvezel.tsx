d9bc8e740a98e705b2db45b4539796720b0c5625
Tjimka Bakker <info@tjimka.nl>
2026-05-31 15:45:40 +0200

    Delete Blog directory

--- Blog/GeenGlasvezel.tsx
+++
@@ -1,8 +0,0 @@
-import BlogPostLayout from "@/components/blog/BlogPostLayout";
-import { getPostBySlug } from "@/data/blogPosts";
-
-const post = getPostBySlug("wat-doe-je-als-er-geen-glasvezel-beschikbaar-is")!;
-
-export default function GeenGlasvezel() {
-  return <BlogPostLayout post={post} />;
-}
