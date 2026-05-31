fe87aface25fe0608dd87d090f8e257fadb7760e
Tjimka Bakker <info@tjimka.nl>
2026-05-31 15:44:03 +0200

    Delete NewlandScanners.tsx

--- NewlandScanners.tsx
+++
@@ -1,8 +0,0 @@
-import BlogPostLayout from "@/components/blog/BlogPostLayout";
-import { getPostBySlug } from "@/data/blogPosts";
-
-const post = getPostBySlug("mobiele-computers-en-scanners-van-newland")!;
-
-export default function NewlandScanners() {
-  return <BlogPostLayout post={post} />;
-}
